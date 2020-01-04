a6dbff31c359a89ed2340d4a4749e75fe8699806
Jiang <15328044115@163.com>
2020-01-04 19:35:55 +0800

    Initial Commit

---
+++ InvocationDemo/InvocationDemo.h
@@ -0,0 +1,19 @@
+//
+//  InvocationDemo.h
+//  InvocationDemo
+//
+//  Created by Jiang on 2020/1/4.
+//  Copyright © 2020 SilverFruity. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InvocationDemo.
+FOUNDATION_EXPORT double InvocationDemoVersionNumber;
+
+//! Project version string for InvocationDemo.
+FOUNDATION_EXPORT const unsigned char InvocationDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InvocationDemo/PublicHeader.h>
+
+
